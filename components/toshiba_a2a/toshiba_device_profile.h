9a7ad28fd8767ef9e06d656d2339f8b3db95945f
insipiens <68975498+insipiens@users.noreply.github.com>
2026-09-18 03:50:34 +0100

    Format declarative family profiles

--- components/toshiba_a2a/toshiba_device_profile.h
+++ components/toshiba_a2a/toshiba_device_profile.h
@@ -1,8 +1,15 @@
 #pragma once
+
 #include <string>
+
 #include "toshiba_family.h"
-namespace esphome { namespace toshiba_a2a {
+
+namespace esphome {
+namespace toshiba_a2a {
+
 const ToshibaFamilyProfile &profile_for_model(const std::string &model);
 const ToshibaFamilyProfile &profile_for_family(ToshibaIndoorUnitFamily family);
 const char *indoor_unit_family_to_string(ToshibaIndoorUnitFamily family);
-} }
+
+}  // namespace toshiba_a2a
+}  // namespace esphome
